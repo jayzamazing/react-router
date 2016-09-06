6da327d0d506e4af71c16282f6f857056134b712
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-09-05 21:03:28 -0400

    Added react route, /contacts route and event listener for routes in public/js/index.js.

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var router = require('react-router');
+var Router = router.Router;
+var Route = router.Route;
+var hashHistory = router.hashHistory;
+
+var routes = (
+    <Router history={hashHistory}>
+        <Route path="/contacts" component={ContactListContainer} />
+    </Router>
+);
+
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(routes, document.getElementById('app'));
+});
 
 var CONTACTS = {
     0: {
